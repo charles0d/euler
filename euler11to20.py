8a1624165e4192716492a992872e6be5f64d2270
charles0d <charles.dutertre@gmail.com>
2021-03-15 23:23:46 +0100

    15 to 19

--- euler11to20.py
+++ euler11to20.py
@@ -1,5 +1,8 @@
 
 
+from num2words import num2words
+
+
 def isprime(x: int):
     for i in range(2, int(x**0.5) + 1):
         if x % i == 0:
@@ -237,4 +240,102 @@ def q14():
     print(i_max, m)
 
 
-q14()
+def factorial(n):
+    return 1 if n == 1 else n * factorial(n-1)
+
+
+def q15():
+    # Simple enumeration question:
+    # the number of such paths is the number of ways there are to set
+    # 20 downards moves out of 40, ie:
+    print(factorial(40)/(factorial(20)**2))
+
+
+def q16():
+    # simple in a high level prg languange:
+    print(sum(list(map(int, list(str(2**1000))))))
+
+
+def q17():
+    count = 0
+    for i in range(1, 1001):
+        n = num2words(i)
+        for a in n:
+            if a.isalpha():
+                count += 1
+    print(count)
+
+
+def q18():
+    # Actually performance is better without the use of dict
+    d = dict()
+    triangle = '75 '\
+               '95 64 '\
+               '17 47 82 '\
+               '18 35 87 10 '\
+               '20 04 82 47 65 '\
+               '19 01 23 75 03 34 '\
+               '88 02 77 73 07 63 67 '\
+               '99 65 04 28 06 16 70 92 '\
+               '41 41 26 56 83 40 80 70 33 '\
+               '41 48 72 33 47 32 37 16 94 29 '\
+               '53 71 44 65 25 43 91 52 97 51 14 '\
+               '70 11 33 28 77 73 17 78 39 68 17 57 '\
+               '91 71 52 38 17 14 91 43 58 50 27 29 48 '\
+               '63 66 04 68 89 53 67 30 73 16 69 87 40 31 '\
+               '04 62 98 27 23 09 70 98 73 93 38 53 60 04 23'\
+
+    triangle = list(map(int, list(triangle.split())))
+
+    def c(row, col):
+        return int(col + row*(row+1)/2)
+
+    def rec(row, col):
+        if row == 14:
+            return triangle[c(row, col)]
+
+        if (row+1, col) in d.keys():
+            left = d[(row+1, col)]
+        else:
+            left = rec(row+1, col)
+            d[(row+1, col)] = left
+
+        if (row+1, col+1) in d.keys():
+            right = d[(row+1, col+1)]
+        else:
+            right = rec(row+1, col+1)
+            d[(row+1, col+1)] = right
+
+        return (triangle[c(row, col)] + max(left, right))
+
+    print(rec(0, 0))
+
+
+def q19():
+    # 1 Jan 1900 = Monday
+    days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    days_sum = [0]
+    for i in days:
+        days_sum.append(days_sum[-1] + i)
+    days_leap = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    days_leap_sum = [0]
+    for i in days_leap:
+        days_leap_sum.append(days_leap_sum[-1] + i)
+
+    def isleap(year):
+        return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+
+    count = 0
+    day1 = 1 % 7
+    for i in range(100):
+        if isleap(1901+i):
+            count += sum(map(lambda x: (x+day1) % 7 == 0, days_leap_sum[:-1]))
+            day1 = (day1 + days_leap_sum[-1]) % 7
+        else:
+            count += sum(map(lambda x: (x+day1) % 7 == 0, days_sum[:-1]))
+            day1 = (day1 + days_sum[-1]) % 7
+
+    print(count)
+
+
+q19()
